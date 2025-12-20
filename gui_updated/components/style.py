07a9e4560c0aac1c933fa142ab9309625771a00a
Mapi <matvey2000000009@gmail.com>
2025-12-20 11:39:45 +0100

    titles in settings

--- gui_updated/components/style.py
+++ gui_updated/components/style.py
@@ -5,6 +5,11 @@ class GuiStyle:
         WHITE = "#ffffff"
         LIGHT_GREEN = "#0d962b"
         GRAY_BLUE = "#1c2f4d"
+        BLUE = "#1d4777"
+        DARK_BLUE = "#0f1c31"
+
+    class TextSize:
+        SETTINGS_TITLE = 20
 
     SETTINGS_SAVE_BUTTON = ft.ButtonStyle(
         color=ft.Colors.WHITE,
@@ -14,5 +19,22 @@ class GuiStyle:
 
     SETTINGS_TOP_BAR_BUTTON = ft.ButtonStyle(
         color=ft.Colors.WHITE,
-        shape=ft.RoundedRectangleBorder(radius=8),
+        bgcolor=ft.Colors.TRANSPARENT,
+        shape=ft.RoundedRectangleBorder(radius=4),
+        text_style=ft.TextStyle(
+            size=TextSize.SETTINGS_TITLE,
+            weight=ft.FontWeight.BOLD,
+        ),
+        elevation=0,
+    )
+
+    SETTINGS_TOP_BAR_BUTTON_SELECTED = ft.ButtonStyle(
+        color=ft.Colors.WHITE,
+        bgcolor=ft.Colors.TRANSPARENT,
+        shape=ft.RoundedRectangleBorder(radius=4),
+        text_style=ft.TextStyle(
+            size=TextSize.SETTINGS_TITLE,
+            weight=ft.FontWeight.BOLD,
+        ),
+        elevation=0,
     )
--- gui_updated/pages/settings.py
+++ gui_updated/pages/settings.py
@@ -14,15 +14,23 @@ class UpperRow(ft.Container):
 
         self.bgcolor = GuiStyle.Colors.GRAY_BLUE
         self.padding = 10
-        self.margin = ft.margin.only(bottom=15)
 
-        self.general_tab_button = ft.ElevatedButton(
-            text="General",
-            style=GuiStyle.SETTINGS_TOP_BAR_BUTTON,
+        general_title = ft.Text(
+            value="General",
+            style=ft.TextStyle(
+                size=GuiStyle.TextSize.SETTINGS_TITLE,
+                color=ft.Colors.WHITE,
+                weight=ft.FontWeight.BOLD,
+            )
         )
+        general_title_row = ft.Container(
+            padding=ft.padding.only(10, 0, 10, 0),
+            content=general_title
+        )
+
         self.fast_buy_tab_button = ft.ElevatedButton(
             text="Fast Buy",
-            style=GuiStyle.SETTINGS_TOP_BAR_BUTTON,
+            style=GuiStyle.SETTINGS_TOP_BAR_BUTTON_SELECTED,
         )
         self.order_buy_tab_button = ft.ElevatedButton(
             text="Order Buy",
@@ -36,22 +44,27 @@ class UpperRow(ft.Container):
         )
 
         self.content = ft.ResponsiveRow(
-            controls=[  
+            controls=[
+                ft.Column(
+                    controls=[
+                        general_title_row
+                    ],
+                    col={"sm": 0, "md": 6, "xl": 4},
+                ),
                 ft.Column(
                     controls= [
                         ft.Row(
                             controls=[
-                                self.general_tab_button,
                                 self.fast_buy_tab_button,
-                                self.order_buy_tab_button
+                                self.order_buy_tab_button,
                             ]
                         )
                     ],
-                    col={"md": 6},
+                    col={"sm": 6, "md": 3, "xl": 4},
                 ),
                 ft.Column(
                     controls=[self.save_button],
-                    col={"md": 6},
+                    col={"sm": 6, "md": 3, "xl": 4},
                     horizontal_alignment=ft.CrossAxisAlignment.END,
                 ),
             ],
@@ -59,39 +72,104 @@ class UpperRow(ft.Container):
         )
 
 
-class GeneralTab(ft.Container):
+class SettingsDropdown(ft.Dropdown):
+    def __init__(self, label: str, tooltip: str, options: list[ft.DropdownOption]):
+        super().__init__(
+            label = label,
+            tooltip = tooltip,
+            options = options,
+            dense=True,
+            expand=True,
+            col={"sm": 12, "md": 6, "xl": 4},
+            filled=True,
+            fill_color=GuiStyle.Colors.GRAY_BLUE,
+            color=ft.Colors.WHITE,
+            text_style=ft.TextStyle(color=ft.Colors.WHITE),
+            label_style=ft.TextStyle(color=ft.Colors.WHITE)
+        )
+
+
+class GeneralSettings(ft.Container):
     def __init__(self):
         super().__init__()
-        self.bgcolor = "#5b0d0d"
+        self.padding = 20
+        self.col={"sm": 12, "md": 6, "xl": 4}
 
-        def get_languages_options():
-            return [ft.DropdownOption(key=language, text=language) for language in AVALIABLE_LANGUAGES]
-
-        self.buy_mode = ft.Dropdown(
-            label="Buy Mode Strategy",
+        self.buy_mode = SettingsDropdown(
+            label="Buy Mode Strategy", 
             tooltip="Choose strategy for bot",
-            value="",
             options=[
                 ft.DropdownOption(text="Order Buy"),
                 ft.DropdownOption(text="Fast Buy")
-            ],
-            dense=True,
+            ]  
         )
 
-        self.game_language = ft.Dropdown(
+        self.game_language = SettingsDropdown(
             label="Language in Game",
             tooltip="Choose your in game Language",
-            value="",
-            options = get_languages_options(),
-            dense=True
+            options = [ft.DropdownOption(key=language, text=language) for language in AVALIABLE_LANGUAGES],
+        )
+
+        self.stop_silver_threshold = ft.TextField(
+            label="Stop if Silver lower Than",
+            tooltip="Bot will stop if your silver will be lower",
+            border_color=GuiStyle.Colors.GRAY_BLUE,
+            color=ft.Colors.WHITE,
+            label_style=ft.TextStyle(color=ft.Colors.WHITE),
+            fill_color=GuiStyle.Colors.DARK_BLUE,
         )
 
         self.content = ft.ResponsiveRow(
             controls=[
-                self.buy_mode,
-                self.game_language
+                ft.Column(
+                    controls = [
+                        self.buy_mode,
+                        self.game_language,
+                        self.stop_silver_threshold,
+                    ],
+                    col=12,
+                )
             ],
-            col={"md": 6}
+        )
+
+
+class FastBuySettings(ft.Container):
+    # Default buy amount
+    # City presets
+    # Buy logic
+    def __init__(self):
+        super().__init__()
+        self.bgcolor = "#2D4F78"
+        self.padding = 20
+        self.col={"sm": 12, "md": 6, "xl": 8}
+
+        self.minimal_profit_rate = ft.TextField(
+            label="Minimal Profit Rate",
+            fill_color=GuiStyle.Colors.DARK_BLUE,
+        )
+
+        self.content = ft.ResponsiveRow(
+            controls=[
+                ft.Column(
+                    controls = [
+                        self.minimal_profit_rate
+                    ],
+                    col={"sm": 12, "md": 8, "xl": 4},
+                )
+            ],
+        )
+
+class SettingsMain(ft.Container):
+    def __init__(self):
+        super().__init__()
+
+        self.content = ft.ResponsiveRow(
+            controls=[
+                GeneralSettings(),
+                FastBuySettings(),     
+            ],
+            spacing=20,
+            run_spacing=20,
         )
 
 
@@ -104,8 +182,9 @@ class Settings(ft.Container):
         self.content = ft.Column(
             controls=[
                 UpperRow(),
-                GeneralTab()
-            ]
+                SettingsMain()
+            ],
+            spacing = 0,
         )
 
 
